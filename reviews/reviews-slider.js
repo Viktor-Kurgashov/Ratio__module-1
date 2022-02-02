893d7a3183773f39339536b8eb4c5f4413789332
Viktor-Kurgashov <e843xx9@gmail.com>
2022-02-03 00:39:09 +0400

    main

---
+++ reviews/reviews-slider.js
@@ -0,0 +1,23 @@
+let stack = document.getElementsByClassName('reviews-list')[0],
+  btnLeft = document.getElementsByClassName('reviews-controls__button')[0],
+  btnRight = document.getElementsByClassName('reviews-controls__button')[1];
+
+function moveRight() {
+  if (window.matchMedia('(max-width: 640px)').matches) {
+    if (!stack.style.marginLeft) stack.style.marginLeft = '-108%'
+    else if (stack.style.marginLeft === '-108%') stack.style.marginLeft = '-216%';
+  }
+  if (window.matchMedia('(max-width: 960px)').matches && (!stack.style.marginLeft)) stack.style.marginLeft = '-52%';
+}
+
+function moveLeft() {
+  if (window.matchMedia('(max-width: 640px)').matches) {
+    if (stack.style.marginLeft === '-108%') stack.style.marginLeft = '';
+    else if (stack.style.marginLeft === '-216%') stack.style.marginLeft = '-108%';
+  }
+  if (window.matchMedia('(max-width: 960px)').matches && (stack.style.marginLeft === '-52%')) stack.style.marginLeft = '';
+}
+
+function reset() { stack.style.marginLeft = ''; }
+
+window.addEventListener('resize', () => reset());
